d5f5888d8b60d43a862264df3d68765afd6269ce
Raivis Kalnins <raivis@63.lv>
2023-04-10 21:46:07 +0100

    home

--- inc/footer-global.php
+++ inc/footer-global.php
@@ -11,6 +11,7 @@ function footer_global() {
 <?php /* if( has_post_thumbnail() ) :  $hero_bg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 <!-- BG Hero -->
 <style>.hero-banner{background-image:url('<?=$hero_bg[0]; ?>');}</style>
+<style>body.login-password-protected,#login,#loginform{background:black;}#loginform{border:0}#wp-submit{background:transparent;border-color:white}#password-protected-logo{display:none}</style>
 <!-- /BG Hero -->
 <?php endif; */ ?>
 <?php edit_post_link(); ?>
